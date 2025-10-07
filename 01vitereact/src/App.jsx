47b3cd62258b085e356a24c2b55b3f88cb6606dd
aksheya kumar <akshaygurjar28803@gmail.com>
2025-10-07 15:01:51 +0530

    react dom

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,10 +1,15 @@
-
+import Chai from "./chai"
 
 function App() {
 
 
-  return (
-  <h1>chai aur react with vite | aksheya kumar vite ko download krna sikha h bhai</h1>
+  return ( // react me ek hi part ko return kr skte h toh isliye hum isko <></> jo ki ek div tag h usme enclose krke bhejte h
+ <>
+    <Chai/>
+    <h1>na na an aanfknfrlenenfnwenfn</h1>
+    <p>paragraph h toh kya h article bhi likh dunga</p>
+ </> // <></> isko fragment bolte h
+ 
   )
 }
 
---
+++ 01vitereact/src/chai.jsx
@@ -0,0 +1,11 @@
+import App from "./App";
+
+function Chai(){
+    return (
+        
+  <h3>chai peeloooo guyssss</h3>
+      
+    )
+}
+
+export default Chai
