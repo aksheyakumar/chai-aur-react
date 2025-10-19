947b6c06e8326fdede90acb1383639ddc92d21d7
aksheya kumar <akshaygurjar28803@gmail.com>
2025-10-19 18:39:07 +0530

    components

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,16 +1,8 @@
-import Chai from "./chai"
-
-function App() {
-
-
-  return ( // react me ek hi part ko return kr skte h toh isliye hum isko <></> jo ki ek div tag h usme enclose krke bhejte h
- <>
-    <Chai/>
-    <h1>na na an aanfknfrlenenfnwenfn</h1>
-    <p>paragraph h toh kya h article bhi likh dunga</p>
- </> // <></> isko fragment bolte h
- 
-  )
+//jsx - jsx sirf dikhta h html ki tarah hota nhi h
+import React from "react";
+function App(){
+    return (
+      <h1>hey</h1> // createElement h function ke bhi use krte h toh hume app ko html se render krte h
+    )   // react me ek hi part ko return kr skte h toh isliye hum isko <></> jo ki ek div tag h usme enclose krke bhejte h
 }
-
-export default App
+export default App;
